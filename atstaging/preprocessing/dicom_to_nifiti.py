acc9bb4b005a0d2f06756d97788e64696be52949
Tom Earnest <31780412+earnestt1234@users.noreply.github.com>
2024-10-24 10:57:46 -0500

    add recursive dcm2niix function

--- atstaging/preprocessing/dicom_to_nifiti.py
+++ atstaging/preprocessing/dicom_to_nifiti.py
@@ -5,6 +5,8 @@ Created on Wed Sep  4 13:53:34 2024
 @author: earne
 """
 
+import os
+from pathlib import Path
 import subprocess
 
 def run_dcm2niix(indir, outdir, name):
@@ -20,3 +22,15 @@ def run_dcm2niix(indir, outdir, name):
         ]
 
     subprocess.run(call)
+
+def recursive_dcm2niix(indir, outdir, name_fmt=r'sub-%i_ses-%t_desc-%p'):
+
+    for root, _, files in os.walk(indir):
+        contains_dicoms = any([f.lower().endswith('dcm') for f in files])
+        if not contains_dicoms:
+            continue
+        relpath = os.path.relpath(root, indir)
+        output_folder = os.path.join(outdir, relpath)
+        Path(output_folder).mkdir(exist_ok=True, parents=True)
+        run_dcm2niix(root, output_folder, name=name_fmt)
+

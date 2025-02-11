920c1ae783badde6c3fe1640d52e65caa8eb413f
earnestt1234 <earnestt1234@gmail.com>
2025-02-10 20:48:45 -0600

    add merge_separated_dicom_frames

--- atstaging/preprocessing/conversion.py
+++ atstaging/preprocessing/conversion.py
@@ -7,6 +7,7 @@ Created on Wed Sep  4 13:53:34 2024
 
 import os
 from pathlib import Path
+import tempfile
 
 import nibabel as nib
 
@@ -20,6 +21,59 @@ def ecat_to_nifti(inimg, outimg):
         ecat.affine
     )
     nib.save(nii, outimg)
+
+def merge_separated_dicom_frames(input_paths, output_path):
+    """
+    Creates a single image from multiple DICOMS, concatenated as frames.
+    This was needed for GS2 data, where some subjects have amyloid
+    PET images saved as 4 separate frames.
+
+    Parameters
+    ----------
+    input_paths : list
+        List of paths to folders which contain DICOM files.  The order provided
+        dictates the order of frames in the resulting image!
+    output_path : str
+        Path to the output image.
+
+    Returns
+    -------
+    None.
+
+    """
+
+    imgs = []
+
+    print('FRAME CONCATENATION')
+    print('===================')
+    print()
+    print(f'> Input images: {len(input_paths)}')
+    print(f'> Output path: {output_path}')
+
+    with tempfile.TemporaryDirectory() as WORKINGDIR:
+        print(f'> Temp directory: {WORKINGDIR}')
+
+        for i, path in enumerate(input_paths):
+
+            print()
+            print(f'- PATH = {path}')
+
+            # convert DCM to NIFTI
+            print('  * Converting to NIFTI.')
+            destname = f'img{i}'
+            destpath = os.path.join(WORKINGDIR, destname + '.nii.gz')
+            run_dcm2niix(path, WORKINGDIR, destname, silent=True)
+
+            # load the NIFTI
+            print('  * Loading NIFTI data.')
+            nii = nib.load(destpath)
+            imgs.append(nii)
+
+        print()
+        print('> Creating concatenated image.')
+        concat = nib.concat_images(images=imgs)
+        nib.save(concat, output_path)
+        print(f'> Done[{output_path}].')
 
 def run_dcm2niix(indir, outdir, name, silent=False):
 

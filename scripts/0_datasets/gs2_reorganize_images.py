bfd180c8dbf18bba6bf4737c3d6b016b4c7feda8
earnestt1234 <earnestt1234@gmail.com>
2025-02-12 12:34:51 -0600

    fix some bugs with GS2 reorganization

--- scripts/0_datasets/gs2_reorganize_images.py
+++ scripts/0_datasets/gs2_reorganize_images.py
@@ -15,44 +15,60 @@ OUTPUT_FOLDER = get('output_directory')
 
 # variables
 gs2_image_dir = '/ceph/chpc/shared/aristeidis_sotiras_group/aris_data/GS2/images/dicom/GS2'
+gs2_allpet_search = '/scratch/tom.earnest/atstaging/searches/gs2_allpet_search.csv'
 output_dir = '/ceph/chpc/shared/aristeidis_sotiras_group/aris_data/GS2/images/nifti/GS2/'
 output_finallist = os.path.join(OUTPUT_FOLDER, 'downloadLists', 'gs2_nifti_images.csv')
 overwrite = False
 merged_pet_name = 'CONCATENATED_PET_IMAGE'
 
-# get list of images
-loni= list_loni_images(gs2_image_dir)
-
 # main script
 print('GS2 Image Reorganization')
+pet = pd.read_csv(gs2_allpet_search, dtype={'Subject ID': str})
 
+print('> Identifying amyloid scans which need merging')
+pet['Subject'] = 'S' + pet['Subject ID']
+pet['ScanDate'] = pd.to_datetime(pet['Study Date'])
+pet['ImageID'] = 'I' + pet['Image ID'].astype(str)
+prot = pet['Imaging Protocol'].str.removeprefix('Radiopharmaceutical=')
+pet['Tracer'] = prot.map(
+    {
+        '18F-Flutemetamol': 'FMT',
+        '18F-Florbetaben': 'FBB',
+        '18F-Florbetapir': 'FBP',
+        '18F-FDG': 'FDG',
+        '18F-AV-1451': 'FTP'
+    }
+)
+amy = pet[pet['Tracer'].isin(['FMT', 'FBB', 'FBP'])].copy()
+amycount = amy.sort_values(['Subject', 'Description']).groupby(['Subject', 'ScanDate'])['Description'].count().reset_index()
+amycount.columns = ['Subject', 'ScanDate', 'DuplicateCount']
+amy = amy.merge(amycount, on=['Subject', 'ScanDate'], how='left')
+n_single = sum(amy['DuplicateCount'].eq(1))
+n_quad = sum(amy['DuplicateCount'].eq(4)) // 4
+print(f'  + Single amyloid images: {n_single}')
+print(f'  + Split amyloid images: {n_quad}')
+
+# get list of downloaded images
 print(f'> Finding images in {gs2_image_dir}')
-# loni = list_loni_images(gs2_image_dir)
+loni = list_loni_images(gs2_image_dir)
+print(f'  + Images found: {len(loni)}')
 
-print('> Identifying cases which need merging')
-# apply some filtering of duplicate reasons
-removed = loni[loni['Path'].str.lower().str.contains('non|not|no_loop')]
-loni = loni[~loni['Path'].str.lower().str.contains('non|not|no_loop')]
-loni = loni.sort_values(['Subject', 'Sequence', 'Date'])
-
-# identify the number of duplicates per case
-# we specficically care in the ones with 4 images
-# by manual inspection, these appear the only cases that need to be merged
-count = loni.groupby(['Subject', 'Date'])['Path'].count().reset_index()
-count.columns = ['Subject', 'Date', 'DuplicateCount']
-loni = loni.merge(count, on=['Subject', 'Date'], how='left')
-n_single = sum(loni['DuplicateCount'].eq(1))
-n_quad = sum(loni['DuplicateCount'].eq(4)) // 4
-print(f'  + Single images: {n_single}')
-print(f'  + Split images: {n_quad}')
+# separate loni into single conversions and quad conversions
+print('> Splitting conversion jobs')
+quads = amy[amy['DuplicateCount'].eq(4)].copy().sort_values(['Subject', 'Description'])
+quads['QuadGroup'] = quads.groupby(['Subject', 'ScanDate']).ngroup()
+merger = quads[['ImageID', 'QuadGroup']]
+loni = loni.merge(merger, how='left', on='ImageID')
+convert_single = loni[loni['QuadGroup'].isna()]
+convert_quad = loni[~loni['QuadGroup'].isna()]
+print(f'  + Single images: {len(convert_single)}')
+print(f'  + Merge images: {len(convert_quad)/4} (4x images each)')
 
 # begin image conversion
 records = []
 
 print('> Converting single images')
-convert_single = loni[loni['DuplicateCount'].eq(1)]
 for i, (idx, row) in enumerate(convert_single.iterrows()):
-
     image = row['Path']
     imageid = os.path.basename(image)
     date = os.path.basename(pdir(image))
@@ -63,16 +79,9 @@ for i, (idx, row) in enumerate(convert_single.iterrows()):
     image_dest_name = acq
     image_dest_path = os.path.join(image_dest_folder, image_dest_name + '.nii.gz')
     
-    print(f'  + [{i+1}/{n_single}]')
+    print(f'  + [{i+1}/{len(convert_single)}]')
     print(f'  + Source: {image}')
     print(f'  + Dest: {image_dest_path}')
-
-    if os.path.isfile(image_dest_path) and not overwrite:
-        print('  + Destination image already exists.')
-        continue
-
-    Path(image_dest_folder).mkdir(parents=True, exist_ok=True)
-    run_dcm2niix(indir=image, outdir=image_dest_folder, name=image_dest_name, silent=True)
 
     record = {
         'Subject': row['Subject'],
@@ -84,9 +93,15 @@ for i, (idx, row) in enumerate(convert_single.iterrows()):
     }
     records.append(record)
 
+    if os.path.isfile(image_dest_path) and not overwrite:
+        print('  + Destination image already exists.')
+        continue
+
+    Path(image_dest_folder).mkdir(parents=True, exist_ok=True)
+    run_dcm2niix(indir=image, outdir=image_dest_folder, name=image_dest_name, silent=True)
+
 print('> Converting split images')
-convert_quad = loni[loni['DuplicateCount'].eq(4)]
-for i, (idx, group) in enumerate(convert_quad.groupby(['Subject', 'Date'])):
+for i, (idx, group) in enumerate(convert_quad.groupby(['QuadGroup'])):
     
     images = sorted(group['Path'])
     example = images[0]
@@ -98,13 +113,9 @@ for i, (idx, group) in enumerate(convert_quad.groupby(['Subject', 'Date'])):
     image_dest_folder = os.path.join(output_dir, sub, merged_pet_name, date, imageid)
     image_dest_path = os.path.join(image_dest_folder, merged_pet_name + '.nii.gz')
 
-    print(f'  + [{i+1}/{n_quad}]')
+    print(f'  + [{i+1}/{len(convert_quad)//4}]')
     print(f'  + Source: {example}')
     print(f'  + Dest: {image_dest_path}')
-
-    if os.path.isfile(image_dest_path) and not overwrite:
-        print('  + Destination image already exists.')
-        continue
 
     row = group.iloc[0, :]
     record = {
@@ -119,6 +130,10 @@ for i, (idx, group) in enumerate(convert_quad.groupby(['Subject', 'Date'])):
         'DICOMPath4': images[3],
     }
     records.append(record)
+
+    if os.path.isfile(image_dest_path) and not overwrite:
+        print('  + Destination image already exists.')
+        continue
 
     Path(image_dest_folder).mkdir(parents=True, exist_ok=True)
     print()

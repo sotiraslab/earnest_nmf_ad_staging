3a168200911546d46a57adc76301bfb9e4a50c23
earnestt1234 <earnestt1234@gmail.com>
2025-05-09 11:53:25 -0500

    fix bug with loadings calculation

--- scripts/4_nmf/calculate_loadings.py
+++ scripts/4_nmf/calculate_loadings.py
@@ -28,19 +28,38 @@ amynmf = load_nmf_runner(os.path.join(output_directory, 'nmf', 'runs', 'amyloid1
 
 # LOAD PATHS TO PREPROCESSED IMAGES
 # filtered to be only the images in the final dataset
-maindata = load_split(None, None)
+maindata = load_split(None, None)[:200]
 paths = load_paths_tables()
 paths = paths[(paths['Subject'] + paths['Session']).isin(maindata['Subject'] + maindata['Session'])].copy()
 
 # TAU LOADINGS
 images = list(paths['tau_registered'])
-tau_loadings = taunmf.compute_loadings_dataframe(rank=tau_rank, images=images, keep_indices=tau_component_indices, keep_names=tau_component_names, prefix='PTC', suffix='SUVR')
+tau_loadings_cache = os.path.join(output_directory, 'masterTables', '_tau_loadings.csv')
+if os.path.isfile(tau_loadings_cache):
+    print()
+    print(f'>  Using cached tau loadings at {tau_loadings_cache}.')
+    tau_loadings = pd.read_csv(tau_loadings_cache)
+else:
+    print()
+    print('>  No cached tau loadings found; recomputing..')
+    tau_loadings = taunmf.compute_loadings_dataframe(rank=tau_rank, images=images, keep_indices=tau_component_indices, keep_names=tau_component_names, prefix='PTC', suffix='SUVR')
+    tau_loadings.to_csv(tau_loadings_cache, index=False)
 
 # AMYLOID LOADINGS
 images = list(paths['amyloid_registered'])
-amy_loadings = amynmf.compute_loadings_dataframe(rank=amy_rank, images=images, keep_indices=amy_component_indices, keep_names=amy_component_names, prefix='PAC', suffix='SUVR')
+amy_loadings_cache = os.path.join(output_directory, 'masterTables', '_amyloid_loadings.csv')
+if os.path.isfile(amy_loadings_cache):
+    print()
+    print(f'>  Using cached amyloid loadings at {amy_loadings_cache}.')
+    amy_loadings = pd.read_csv(amy_loadings_cache)
+else:
+    print()
+    print('>  No cached amyloid loadings found; recomputing..')
+    amy_loadings = amynmf.compute_loadings_dataframe(rank=amy_rank, images=images, keep_indices=amy_component_indices, keep_names=amy_component_names, prefix='PTC', suffix='SUVR')
+    amy_loadings.to_csv(amy_loadings_cache, index=False)
 
 # CREATE FEATURES
-features = pd.concat([paths[['Subject', 'Session']], tau_loadings, amy_loadings], axis=1)
+# reset index is used to ensure we are just pasting things in the same order as the images being iterated over
+features = pd.concat([paths[['Subject', 'Session']].reset_index(drop=True), tau_loadings.reset_index(drop=True), amy_loadings.reset_index(drop=True)], axis=1)
 outpath = os.path.join(output_directory, 'masterTables', 'FEATURE_NMFLOADINGS.csv')
 features.to_csv(outpath, index=False)

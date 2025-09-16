9d864341dfe968bbbe34fb62a5a67d2e9f09b718
Tom Earnest <31780412+earnestt1234@users.noreply.github.com>
2025-09-16 10:32:24 -0500

    Update model_tau_vs_amyloid.R

--- scripts/6_sustain/subtype_characterization/model_tau_vs_amyloid.R
+++ scripts/6_sustain/subtype_characterization/model_tau_vs_amyloid.R
@@ -13,39 +13,52 @@ dir.create(odir, showWarnings = F)
 master.path <- file.path(ROOT.OUTPUT, 'filesForR', 'master_with_sustain.csv')
 master <- read.csv(master.path)
 
-pacs <- colnames(master)[str_detect(colnames(master), 'PAC.*WScore')]
 ptcs <- colnames(master)[str_detect(colnames(master), 'PTC.*WScore')]
 
 training <- master %>%
   filter(Split == 'TrainingBaseline', ControlForStaging == 'False', TrainingMLStage != 0, TrainingSubtypeValid == 1) %>%
   mutate(Subtype = TrainingMLSubtype)
 
-plots <- vector(mode = 'list', length = length(pacs))
+validation <- master %>%
+  filter(Split == 'ValidationBaseline', ControlForStaging == 'False', TrainingMLStage != 0, TrainingSubtypeValid == 1) %>%
+  mutate(Subtype = TrainingMLSubtype)
 
-colors <- c(
-  'NA' = 'gray',
-  'S1' = '#db2b39',
-  'S2' = '#053c5e',
-  'S3' = '#f3a712' 
-)
-
-for (i in 1:length(ptcs)) {
-  ptc <- ptcs[i]
-  title <- ptc %>% str_replace('WScore', '') %>% str_replace('PTC', 'PTC-')
-  p <- ggplot(data=training, aes(x = SummarySUVRAmyloid, y = !!sym(ptc), color = Subtype)) +
-    geom_smooth(method = 'lm') +
-    coord_cartesian(ylim = c(0, 20)) +
-    theme_bw() +
-    scale_color_manual(values = colors) +
-    theme(text = element_text(size = 12), plot.title = element_text(size = 12)) + 
-    ggtitle(title) +
-    ylab('W') +
-    xlab('Amyloid (SUVR)')
-  plots[[i]] <- p
+pipeline <- function(data) {
+  plots <- vector(mode = 'list', length = length(pacs))
+  
+  colors <- c(
+    'NA' = 'gray',
+    'S1' = '#db2b39',
+    'S2' = '#053c5e',
+    'S3' = '#f3a712' 
+  )
+  
+  for (i in 1:length(ptcs)) {
+    ptc <- ptcs[i]
+    title <- ptc %>% str_replace('WScore', '') %>% str_replace('PTC', 'PTC-')
+    p <- ggplot(data=data, aes(x = SummarySUVRAmyloid, y = !!sym(ptc), color = Subtype)) +
+      geom_smooth(method = 'lm') +
+      coord_cartesian(ylim = c(0, 20)) +
+      theme_bw() +
+      scale_color_manual(values = colors) +
+      theme(text = element_text(size = 12), plot.title = element_text(size = 12)) + 
+      ggtitle(title) +
+      ylab('W') +
+      xlab('Amyloid (SUVR)')
+    plots[[i]] <- p
+  }
+  
+  p <- ggarrange(plotlist = plots, common.legend = TRUE, legend="top", labels = NULL)
+  print(p)
+  return (p)
 }
 
-ggarrange(plotlist = plots, common.legend = TRUE, legend="top", labels = NULL)
+
+p <- pipeline(training)
 ggsave(file.path(odir, 'training_tau_v_amyloid.svg'), width = 8, height = 8, units = "in", dpi = 300)
+
+p <- pipeline(validation)
+ggsave(file.path(odir, 'validation_tau_v_amyloid.svg'), width = 8, height = 8, units = "in", dpi = 300)
 
 # m <- lm(PTCOccipitalWScore ~ SummarySUVRAmyloid*Subtype, data=training)
 # summary(m)

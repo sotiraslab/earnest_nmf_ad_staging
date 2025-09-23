79a752b82019ed4fb2f354063fe6b5229d93f637
Tom Earnest <31780412+earnestt1234@users.noreply.github.com>
2025-09-23 12:48:47 -0500

    add comparison of atstage and subtype

--- scripts/6_sustain/subtype_characterization/versus_typical_staging.R
+++ scripts/6_sustain/subtype_characterization/versus_typical_staging.R
@@ -1,8 +1,10 @@
 library(dplyr)
-library(gt)
-library(gtsummary)
+library(ggalluvial)
+library(ggplot2)
 library(lubridate)
 library(stringr)
+library(this.path)
+library(tidyr)
 
 # Set where to find files
 ROOT.OUTPUT <- '/Users/earnestt1234/Desktop/atstaging/'
@@ -13,7 +15,15 @@ PATH.OUTPUT <- file.path(ROOT.OUTPUT, 'tables')
 dir.create(PATH.OUTPUT, showWarnings = F)
 
 master <- read.csv(PATH.DATA)
-master$TauAmyloidMeanDate <- as_datetime(ymd_hms(master$TauAmyloidMeanDate))
+master$Subtype <- factor(master$TrainingMLSubtype, levels = c('S1', 'S2', 'S3'))
+master$ATStage <- master$Stage
+
+odir <- file.path(ROOT.OUTPUT, 'plots', 'sustain', 'sustain_vs_atstaging')
+dir.create(odir, showWarnings = F)
+
+# Load plot script
+path.script <- normalizePath(file.path(this.dir(), '..', '..', 'rsource', 'stacked_barplot.R'))
+source(path.script)
 
 # data selection
 training <- master %>%
@@ -32,4 +42,89 @@ validation <- master %>%
     TrainingSubtypeValid ==1 
   )
 
-# Plot 
+# Plot
+
+pipeline <- function(data, name) {
+  
+  atcolors <- list(
+    A0T0 = 'white',
+    A1T0 = '#5fabf7',
+    A2T0 = '#1f4ad8',
+    A2T1 = '#fee187',
+    A2T2 = '#feab49',
+    A2T3 = '#fc5b2e',
+    A2T4 = '#d41020',
+    Atypical = '#A661C9',
+    'Other'= 'gray',
+    'A0T+'= '#c6c7e1',
+    'A1T+'= '#796eb2',
+    'MTL-'= '#3f007d'
+  )
+  
+  scolors <- list(
+    'NA' = 'gray',
+    'S1' = '#db2b39',
+    'S2' = '#053c5e',
+    'S3' = '#f3a712' 
+  )
+  
+  atcolors <- unlist(atcolors)
+  scolors <- unlist(scolors)
+  colors <- c(atcolors, scolors)
+  
+  # Alluvial plot
+  p.data <- data %>%
+    select(Subject, Subtype, ATStage) %>%
+    pivot_longer(-Subject, names_to = 'Model', values_to = 'Label') %>%
+    mutate(
+      Model = factor(Model, levels = c('ATStage', 'Subtype'))
+    )
+  
+  ggplot(data = p.data, aes(x = Model, stratum = Label, alluvium = Subject, fill = Label, label = Label)) +
+    geom_flow(stat = "flow", aes.flow='forward') +
+    geom_stratum(width=1/2) +
+    theme_classic() +
+    scale_y_continuous(expand = c(0, 0.005)) +
+    scale_x_discrete(expand = c(0.13, 0.13)) +
+    theme(
+      axis.line.y = element_blank(),
+      axis.text.y = element_blank(),
+      axis.ticks.y = element_blank(),
+      text = element_text(size = 14)
+    ) +
+    scale_fill_manual(values = colors)
+  
+  ggsave(file.path(odir, sprintf('%s_alluvial_comparison.svg', name)), width = 6, height = 8, units = 'in')
+  
+  # Stacked bar 
+  stacked.barplot(
+    df = data,
+    xcol = 'Subtype',
+    ycol = 'ATStage',
+    colors = atcolors,
+    annotate = T,
+    annotate.size = 5,
+    annotation.minsize = 2)
+  
+  ggsave(file.path(odir, sprintf('%s_barplot_atstage.svg', name)), width = 6, height = 8, units = 'in')
+  
+  # Show atypical cases
+  atypical <- data %>%
+    filter(ATStage == 'Atypical')
+  
+  stacked.barplot(
+    df = atypical,
+    xcol = 'StageLabelNS',
+    ycol = 'Subtype',
+    colors = colors,
+    annotate = T,
+    annotate.color = 'white',
+    annotate.size = 5
+  ) + ylab('Atypical Stage')
+  
+  ggsave(file.path(odir, sprintf('%s_barplot_atypical.svg', name)), width = 6, height = 8, units = 'in')
+  
+}
+
+pipeline(training, 'training')
+pipeline(validation, 'validation')
---
+++ scripts/rsource/stacked_barplot.R
@@ -0,0 +1,99 @@
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+
+stacked.barplot <- function(df, xcol, ycol, levels=NULL, colors=NULL,
+                            return.data = F, dropna = F, annotate = F,
+                            annotate.color = 'black',
+                            annotate.size = 6, annotation.minsize = 0) {
+  
+  # create data
+  if (dropna) {
+    df <- df[! is.na(df[[xcol]]), ]
+    df <- df[! is.na(df[[ycol]]), ]
+  }
+  
+  plot.data <- df %>%
+    group_by_at(c(xcol, ycol)) %>%
+    summarise(N=n())
+  
+  if (! is.null(levels)) {
+    plot.data <- filter(plot.data, !!sym(xcol) %in% levels)
+  }
+  plot.data <- plot.data %>%
+    ungroup() %>%
+    group_by(!!sym(xcol)) %>%
+    mutate(Percent = N / sum(N) * 100)
+  
+  if (return.data) {
+    return (plot.data)
+  }
+  
+  # annotations
+  x.order <- sort(unique(plot.data[[xcol]]))
+  y.order <- sort(unique(plot.data[[ycol]]))
+  annot.data <- plot.data %>%
+    mutate(XFactor = factor(!!sym(xcol), levels = x.order),
+           YFactor = factor(!!sym(ycol), levels = rev(y.order))) %>%
+    arrange(XFactor, YFactor) %>%
+    mutate(AnnotX = as.integer(XFactor)) %>%
+    group_by(XFactor) %>%
+    mutate(AnnotYMax = cumsum(Percent),
+           AnnotYMin = AnnotYMax - Percent,
+           AnnotY = (AnnotYMin + AnnotYMax) / 2,
+           Annot = str_c(round(Percent, 2), '%'),
+           Annot = ifelse((AnnotYMax - AnnotYMin) < annotation.minsize, '', Annot)
+           )
+  
+  # try to spread out some of the annotations
+  # if (! is.null(annotation.closeness)) {
+  #   for (x in x.order) {
+  #     mask <- annot.data[[xcol]] == x
+  #     yloc <- annot.data[mask, 'AnnotY', drop = T]
+  #     too.close <- diff(yloc) < annotation.closeness
+  #     
+  #     # all are far enough apart
+  #     if (! any(too.close)) {
+  #       next
+  #     }
+  #     
+  #     # some are too close, move
+  #     annot.data[mask, 'AnnotY'] <- ifelse(c(F, too.close), yloc + (annotation.closeness/2), yloc)
+  #     annot.data[mask, 'AnnotY'] <- ifelse(c(too.close, F), yloc - (annotation.closeness/2), annot.data[mask, 'AnnotY', drop = T])
+  #   }
+  # }
+  
+  # get totals for text
+  group.sums <- plot.data %>%
+    group_by(!!sym(xcol)) %>%
+    summarise(total=sum(N)) %>%
+    ungroup()
+  
+  # plot
+  p <- ggplot() +
+    geom_bar(data = plot.data, aes(fill=!!sym(ycol), y=Percent, x=!!sym(xcol)), stat="identity", color='black') +
+    geom_text(data = group.sums, aes(x=!!sym(xcol), y=105, label=total), size=6) +
+    theme_classic() +
+    ylab('Observations (%)') +
+    xlab(xcol) +
+    scale_y_continuous(expand=expansion(mult=c(0, .1)), breaks = c(0, 25, 50, 75, 100)) +
+    guides(fill=guide_legend(title=ycol)) +
+    theme(text = element_text(size=20),
+          axis.line.y = element_blank()) +
+    geom_segment(aes(y=0,yend=100,x=-Inf,xend=-Inf), color='black', linewidth=1)
+  
+  if (annotate) {
+    p <- p + geom_text(
+      data = annot.data,
+      aes(x = AnnotX, y = AnnotY, label = Annot),
+      inherit.aes = F,
+      color = annotate.color,
+      size = annotate.size)
+  }
+  
+  if (! is.null(colors)) {
+    p <- p + scale_fill_manual(values=colors)
+  }
+  
+  return(p)
+}
